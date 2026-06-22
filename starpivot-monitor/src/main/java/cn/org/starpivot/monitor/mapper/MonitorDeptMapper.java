cc019853c8e28ebc53e752eda13ef90affa7ce1f
xin1998 <1092203240@qq.com>
2026-06-22 15:42:39 +0800

    fix: sql放到xml文件中

--- starpivot-monitor/src/main/java/cn/org/starpivot/monitor/mapper/MonitorDeptMapper.java
+++ starpivot-monitor/src/main/java/cn/org/starpivot/monitor/mapper/MonitorDeptMapper.java
@@ -3,7 +3,6 @@ package cn.org.starpivot.monitor.mapper;
 import cn.org.starpivot.monitor.domain.entity.MonitorDept;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.Collection;
 import java.util.List;
@@ -22,15 +21,6 @@ public interface MonitorDeptMapper {
      * @param deptIds 部门 ID 集合
      * @return 部门列表
      */
-    @Select("""
-            <script>
-            SELECT dept_id AS deptId, dept_name AS deptName
-            FROM sys_dept
-            WHERE dept_id IN
-            <foreach collection='deptIds' item='id' open='(' separator=',' close=')'>#{id}</foreach>
-            AND del_flag = '0'
-            </script>
-            """)
     List<MonitorDept> selectByDeptIds(@Param("deptIds") Collection<Long> deptIds);
 
     /**
@@ -39,6 +29,5 @@ public interface MonitorDeptMapper {
      * @param deptId 部门 ID
      * @return 部门信息，不存在时返回 {@code null}
      */
-    @Select("SELECT dept_id AS deptId, dept_name AS deptName FROM sys_dept WHERE dept_id = #{deptId} AND del_flag = '0'")
     MonitorDept selectByDeptId(@Param("deptId") Long deptId);
 }
--- starpivot-monitor/src/main/java/cn/org/starpivot/monitor/mapper/MonitorUserMapper.java
+++ starpivot-monitor/src/main/java/cn/org/starpivot/monitor/mapper/MonitorUserMapper.java
@@ -3,7 +3,6 @@ package cn.org.starpivot.monitor.mapper;
 import cn.org.starpivot.monitor.domain.entity.MonitorUser;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.Collection;
 import java.util.List;
@@ -22,15 +21,6 @@ public interface MonitorUserMapper {
      * @param userIds 用户 ID 集合
      * @return 用户列表
      */
-    @Select("""
-            <script>
-            SELECT user_id AS userId, user_name AS userName, nick_name AS nickName, dept_id AS deptId
-            FROM sys_user
-            WHERE user_id IN
-            <foreach collection='userIds' item='id' open='(' separator=',' close=')'>#{id}</foreach>
-            AND del_flag = '0'
-            </script>
-            """)
     List<MonitorUser> selectByUserIds(@Param("userIds") Collection<Long> userIds);
 
     /**
@@ -39,9 +29,5 @@ public interface MonitorUserMapper {
      * @param userId 用户 ID
      * @return 用户信息，不存在时返回 {@code null}
      */
-    @Select("""
-            SELECT user_id AS userId, user_name AS userName, nick_name AS nickName, dept_id AS deptId
-            FROM sys_user WHERE user_id = #{userId} AND del_flag = '0'
-            """)
     MonitorUser selectByUserId(@Param("userId") Long userId);
 }
